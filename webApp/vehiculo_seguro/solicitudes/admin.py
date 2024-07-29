ba3f4a337f8dba8f942b40020c2544ea99c0cc85
Emilio Ferreyra <emilioferreyra@gmail.com>
2024-07-29 15:43:09 -0400

    feat(admin): habilitar solicitudes en el sitio de administracion

--- webApp/vehiculo_seguro/solicitudes/admin.py
+++ webApp/vehiculo_seguro/solicitudes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Solicitud
+
+# Registro de la vista de administración de solicitudes
+@admin.register(Solicitud)
+class SolicitudAdmin(admin.ModelAdmin):
+    list_display = ('tipo_vehiculo', 'numero_identificacion', 'nombre_solicitante', 'email_solicitante', 'estado', 'fecha_emision', 'fecha_expiracion')
+    list_filter = ('estado',)
+    search_fields = ('tipo_vehiculo', 'numero_identificacion', 'nombre_solicitante', 'email_solicitante')
+    ordering = ('fecha_emision',)
